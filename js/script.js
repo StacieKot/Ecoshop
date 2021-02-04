90bbd21826349a8349df53312bd66c46a5f20dff
HP-U004UR\HP <stacie.kot@gmail.com>
2021-02-04 23:00:31 +0300

    04.02.21 23.00

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ formElement2.onsubmit = validateSubscriptionForm;
 
 function validateSubscriptionForm() {
    var email = formElement2.elements.email;
-
    var emailValue = email.value;
-
    if (emailValue.indexOf('@') === -1 || emailValue.indexOf('.') === -1) {
       alert("Введите корректный email");
       return false;
@@ -20,3 +18,24 @@ function loadArticle3() {
 function loadArticle2() {
    $('#article2').load("article2.html");
 }
+
+
+var formLogin=document.forms.loginform;
+formLogin.onsubmit = validateLoginForm;
+function validateLoginForm(){
+    var userName = formLogin.elements.username;
+    var password = formLogin.elements.password;
+    var userNameValue = userName.value;
+    var passwordValue = password.value;
+    if(userNameValue.length<1){
+      alert("Введите ваше имя");
+      userName.focus();
+      return false;
+    }
+    if(passwordValue.length<1){
+      alert("Введите пароль");
+      password.focus();
+      return false;
+    }
+    return true;
+ }
